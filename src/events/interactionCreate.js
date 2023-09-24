d197a082db2e7d60f86d25de4fdd677e2668128d
Leo Gavin <leogavin123@outlook.com>
2023-09-24 11:56:16 +0530

    edit the embeded when user click on button

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,9 +1,9 @@
-const { EmbedBuilder, MessageActionRow, MessageButton,ButtonBuilder,ActionRowBuilder } = require('discord.js');
+const { EmbedBuilder, MessageActionRow, MessageButton,ButtonBuilder,ActionRowBuilder, InteractionCollector } = require('discord.js');
 
 module.exports = {
   name: 'interactionCreate',
   async execute(interaction, client) {
-    if (!interaction.isCommand() && !interaction.isModalSubmit()) return;
+    if (!interaction.isCommand() && !interaction.isModalSubmit() && !interaction.isButton()) return;
 
     // Handle command interactions
     if (interaction.isCommand()) {
@@ -17,19 +17,27 @@ module.exports = {
     }
 
     // Handle modal submission
-if (interaction.isModalSubmit()) {
+  if (interaction.isModalSubmit()) {
   if (interaction.customId === 'pollModal') {
     const eventName = interaction.components[0].components[0].value;
     const eventDescription =interaction.components[1].components[0].value;
     const choices = interaction.components[2].components[0].value.split(',');
       // Extracting the event description
 
+      // Create an array of fields for the choices
+      const choiceFields = choices.map((choice, index) => {
+        return { name: choice, value: ' ', inline: true };
+      });
+
     // Create the poll embed using EmbedBuilder
     const pollEmbed = new EmbedBuilder()
       .setTitle(eventName)
       .setDescription(`Description: ${eventDescription}\n\nPlease vote by commenting below.`)
       .setColor(0x0099FF)
-      .setTimestamp();
+      .setTimestamp()
+      .addFields(
+        ...choiceFields,
+      );
 
       //Present, Absent, Late, I don't know
       // Create buttons for each choice using ButtonBuilder
@@ -66,5 +74,59 @@ if (interaction.isModalSubmit()) {
    
   }
     }
+
+     
+// Handle button interactions
+if (interaction.isButton()) {
+  try {
+    const customIdObject = JSON.parse(interaction.customId);
+    const username = interaction.user.username;
+
+    // Fetch the message containing the embed
+    const message = await interaction.channel.messages.fetch({ around: interaction.message.id, limit: 1 });
+    const fetchedMessage = message.first();
+
+    // Get the existing embed data
+    const receivedEmbed = fetchedMessage.embeds[0];
+    const { title, description, color, timestamp, fields } = receivedEmbed;
+
+    // Create a new EmbedBuilder
+    const newEmbed = new EmbedBuilder()
+      .setTitle(title)
+      .setDescription(description)
+      .setColor(color);
+
+    // Convert the timestamp string to a Date object and set it
+    const dateTimestamp = new Date(timestamp);
+    newEmbed.setTimestamp(dateTimestamp);
+
+    // Update the fields
+    const index = parseInt(customIdObject.ffb.split('_')[1]);
+    fields[index].value = username;  // Set the username as the value
+
+    // Ensure all field values are non-empty
+    fields.forEach(field => {
+      if (!field.value || field.value.length === 0) {
+        field.value = " ";  // Set to a space if the value is empty
+      }
+    });
+
+    // Add the fields to the new embed
+    newEmbed.addFields(fields);
+
+    // Edit the message to update the embed
+    await fetchedMessage.edit({ embeds: [newEmbed] });
+
+    await interaction.reply(`User ${username} voted for ${fields[index].name}.`);
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+}
+
+
+
   },
+  
 };
+
+
