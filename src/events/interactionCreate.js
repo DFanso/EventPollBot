4dadedef34f82ae74d45f7b770e5c9479b60c4db
Leo Gavin <leogavin123@outlook.com>
2023-09-24 18:36:04 +0530

    choice changing logic done

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,4 +1,5 @@
 const { EmbedBuilder, MessageActionRow, MessageButton,ButtonBuilder,ActionRowBuilder, InteractionCollector } = require('discord.js');
+const userButtonMap = {};
 
 module.exports = {
   name: 'interactionCreate',
@@ -26,7 +27,7 @@ module.exports = {
 
       // Create an array of fields for the choices
       const choiceFields = choices.map((choice, index) => {
-        return { name: choice, value: ' ', inline: true };
+        return { name: choice, value: ' ', inline: false };
       });
 
     // Create the poll embed using EmbedBuilder
@@ -75,68 +76,64 @@ module.exports = {
   }
     }
 
-     
-// Initialize the user-button map
-const userButtonMap = {};
+    
 
 // Handle button interactions
 if (interaction.isButton()) {
   try {
     const customIdObject = JSON.parse(interaction.customId);
     const userId = interaction.user.id;
-
-    // Create the user mention string
     const userMention = `<@${userId}>`;
 
-    // Fetch the message containing the embed
     const message = await interaction.channel.messages.fetch({ around: interaction.message.id, limit: 1 });
     const fetchedMessage = message.first();
-
-    // Get the existing embed data
     const receivedEmbed = fetchedMessage.embeds[0];
-    const { title, description, color, timestamp, fields } = receivedEmbed;
+    const { fields } = receivedEmbed;
 
-    // Create a new EmbedBuilder
-    const newEmbed = new EmbedBuilder()
-      .setTitle(title)
-      .setDescription(description)
-      .setColor(color)
-      .setTimestamp(new Date(timestamp));
+    // Debugging: Log the current state of fields and userButtonMap
+    console.log("Before:", { fields, userButtonMap });
 
-    // Check if the user has previously clicked a button
-    if (userButtonMap[userId] !== undefined) {
-      const prevIndex = userButtonMap[userId];
-      // Remove the user mention from the previous field
-      const prevValues = fields[prevIndex].value.split('\n');
-      fields[prevIndex].value = prevValues.filter(val => val !== userMention).join('\n');
-    }
+    // Remove previous choice if exists
+  if (userButtonMap.hasOwnProperty(userId)) {
+    const prevIndex = userButtonMap[userId];
+    const prevUsers = fields[prevIndex].value.split(' ');
+    const newPrevUsers = prevUsers.filter(u => u !== userMention).join(' ');
+    fields[prevIndex].value = newPrevUsers;
+  }
 
-    // Update the fields
-    const index = parseInt(customIdObject.ffb.split('_')[1]);
-    fields[index].value += `${userMention}\n`;
+  // Add new choice
+  const index = parseInt(customIdObject.ffb.split('_')[1]);
+  fields[index].value = fields[index].value ? fields[index].value + ' ' + userMention : userMention;
 
-    // Update the user-button map
-    userButtonMap[userId] = index;
+  // Update the user-button map
+  userButtonMap[userId] = index;
 
-    // Ensure all field values are non-empty
+    // Debugging: Log the updated state of fields and userButtonMap
+    console.log("After:", { fields, userButtonMap });
+
+    // Ensure all field values are non-empty and trim extra spaces
     fields.forEach(field => {
       if (!field.value || field.value.length === 0) {
         field.value = " ";
+      } else {
+        field.value = field.value.trim();
       }
     });
 
-    // Add the fields to the new embed
-    newEmbed.addFields(fields);
+    // Create a new EmbedBuilder and add the updated fields
+    const newEmbed = new EmbedBuilder()
+      .setTitle(receivedEmbed.title)
+      .setDescription(receivedEmbed.description)
+      .setColor(receivedEmbed.color)
+      .setTimestamp(new Date(receivedEmbed.timestamp))
+      .addFields(fields);
 
-    // Edit the message to update the embed
     await fetchedMessage.edit({ embeds: [newEmbed] });
 
-    await interaction.reply(`User ${userMention} voted for ${fields[index].name}.`);
   } catch (error) {
     console.error("An error occurred:", error);
   }
 }
-
 
 
 
