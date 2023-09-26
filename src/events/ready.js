13f22f802fa7826da5d9070a0f218dc86aab7be1
Leo Gavin <leogavin123@outlook.com>
2023-09-26 17:05:44 +0530

    bug fix

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,7 +4,6 @@ module.exports = {
     name: 'ready',
     execute(client) {
         client.user.setActivity('message', { type: ActivityType.Listening });
-        client.user.setStatus('dnd');
         client.user.setAvatar('https://d2cbg94ubxgsnp.cloudfront.net/Pictures/2000xAny/9/9/2/512992_shutterstock_715962319converted_749269.png');
       console.log('Bot is online!');
     },
